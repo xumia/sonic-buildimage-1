c61b2d9c50fcce4fa902656bc201a28c45c57cea
qiluo <qiluo@>
2015-10-28 15:35:55 -0700

    Fix home directory ownership in the file system cpio package

--- build_image.sh
+++ build_image.sh
@@ -3,7 +3,8 @@
 DEMO_SYSROOT_CPIO=fsroot.cpio
 DEMO_SYSROOT_CPIO_XZ=demo.initrd
 
-fakeroot -- ./make-sysroot.sh ./make-devices.pl fsroot ../$DEMO_SYSROOT_CPIO
+## Note: Do NOT use fakeroot because 'cpio --create' will lose file and directory ownership
+sudo ./make-sysroot.sh ./make-devices.pl fsroot ../$DEMO_SYSROOT_CPIO
 xz --compress --force --check=crc32 --stdout -8 $DEMO_SYSROOT_CPIO > $DEMO_SYSROOT_CPIO_XZ
 CONSOLE_SPEED=9600 \
 CONSOLE_DEV=0 \
