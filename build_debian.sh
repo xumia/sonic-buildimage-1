80ffa930ce13711583a283d268060dd7dfd9f82b
qiluo <qiluo@>
2015-12-03 15:00:00 -0800

    Revert "Remove deps of boot_part.py" The packages gdisk/parted/grub are needed by ONIE image to run inside ACS-OS
    This reverts commit fea4de3ad67f3ee398d9527cc7a9b20e72b41ca4.

--- build_debian.sh
+++ build_debian.sh
@@ -86,7 +86,12 @@ sudo LANG=C chroot $FILESYSTEM_ROOT useradd -G sudo $DEFAULT_USERNAME -c "$DEFAU
 sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo $DEFAULT_USERNAME:$DEFAULT_PASSWORD | chpasswd -e"
 
 ## Pre-install the fundamental packages
-sudo LANG=C chroot $FILESYSTEM_ROOT apt-get -y install sudo vim screen tcpdump ntp openssh-server python python-apt
+sudo LANG=C chroot $FILESYSTEM_ROOT apt-get -y install sudo vim screen tcpdump ntp openssh-server python python-apt \
+        gdisk parted
+
+## Pre-install grub for image OS future partition manipulation
+## Note: DEBIAN_FRONTEND is needed to prvent interactive configuration for grub-pc
+sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y install grub-pc grub2
 
 echo '[INFO] install apt-transport-sftp package for azure repository'
 sudo LANG=C chroot $FILESYSTEM_ROOT apt-get -y install libssh2-1
