56747ceeeaa71fc09c105a905e7027ddfc7e99e9
qiluo <qiluo@>
2015-12-09 19:13:24 -0800

    Build acs debs into docker image

--- build_docker.sh
+++ build_docker.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 ## This script is to automate the preparation for a docker image
+## Usage:
+##   sudo ./build_docker.sh DOCKER_IMAGE_GZ
+
 
 set -x -e
 
 DOCKER_IMAGE_TAG=acs-docker-tag
 
 ## File name for docker image
-docker_iamge_gz=acs-docker.gz
+docker_image_gz=acs-docker.gz
 
-[ -n "$docker_iamge_gz" ] || {
-    echo "Error: Invalid git revisions"
+[ -n "$docker_image_gz" ] || {
+    echo "Error: Outut docker image filename is empty"
     exit 1
 }
 
 function cleanup {
+    rm -rf acs-docker/deps
     #sudo docker stop $DOCKER_IMAGE_TAG || true
     #sudo docker rm $DOCKER_IMAGE_TAG || true
-    sudo docker rmi -f $DOCKER_IMAGE_TAG
+    sudo docker rmi -f $DOCKER_IMAGE_TAG || true
 }
 trap cleanup exit
 
+## Note Dockerfile ADD doesn't support reference files outside the folder, so copy it locally
+mkdir -p acs-docker/deps
+cp deps/*.deb acs-docker/deps
 sudo docker build -t $DOCKER_IMAGE_TAG acs-docker
-sudo docker save $DOCKER_IMAGE_TAG | gzip -c > $docker_iamge_gz
+sudo docker save $DOCKER_IMAGE_TAG | gzip -c > $docker_image_gz
 
-#gunzip -c $docker_iamge_gz | sudo docker load
+#gunzip -c $docker_image_gz | sudo docker load
 #sudo docker run -it --name $DOCKER_IMAGE_TAG -v /home:/home $DOCKER_IMAGE_TAG /bin/bash
