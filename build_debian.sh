86c09b055ea527bc7369b847d3b067f392c0eed5
qiluo <qiluo@>
2016-01-05 19:19:25 -0800

    pip uninstall without prompt

--- build_debian.sh
+++ build_debian.sh
@@ -136,7 +136,7 @@ sudo LANG=C chroot $FILESYSTEM_ROOT apt-get -y install      \
 sudo LANG=C chroot $FILESYSTEM_ROOT easy_install pip
 sudo LANG=C chroot $FILESYSTEM_ROOT pip install 'docker-py==1.6.0'
 ## Remove pip which is unnecessary in the base image
-sudo LANG=C chroot $FILESYSTEM_ROOT pip uninstall pip
+sudo LANG=C chroot $FILESYSTEM_ROOT pip uninstall -y pip
     
 ## Pre-install grub for image OS future partition manipulation
 ## Note: DEBIAN_FRONTEND is needed to prvent interactive configuration for grub-pc
