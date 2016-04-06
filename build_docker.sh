700d2b7c4a239f06ed7a72709b718e344e7937e7
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-04-05 17:30:22 -0700

    Reuse base docker image

--- build_docker.sh
+++ build_docker.sh
@@ -32,7 +32,7 @@ docker_image_gz=$docker_image_name.gz
 function cleanup {
     rm -rf $DOCKER_BUILD_DIR/files
     rm -rf $DOCKER_BUILD_DIR/deps
-    docker rmi -f $docker_image_name || true
+    docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi | true
 }
 trap cleanup exit
 
@@ -47,11 +47,22 @@ mkdir -p $DOCKER_BUILD_DIR/files
 cp files/sources.list $DOCKER_BUILD_DIR/files
 docker build -t $docker_image_name $DOCKER_BUILD_DIR
 
+## Flatten the image by importing an exported container on this image
+## Note: it will squash the image with only one layer and lost all metadata such as ENTRYPOINT,
+##       so apply only to the base image
+## TODO: wait docker-squash supporting Docker 1.10+
+## ref: https://github.com/jwilder/docker-squash/issues/45
+if [ "$docker_image_name" = "docker-base" ]; then
+    tmp_container=$(docker run -d ${docker_image_name} /bin/bash)
+    docker export $tmp_container | docker import - ${docker_image_name}
+    docker rm -f $tmp_container || true
+fi
+
 if [ -n "$REGISTRY_SERVER" ] && [ -n "$REGISTRY_PORT" ]; then
     ## Add registry information as tag, so will push as latest
     ## Temporarily add -f option to prevent error message of Docker engine version < 1.10.0
     docker tag -f $docker_image_name $REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name
-    
+
     ## Login the docker image registry server
     ## Note: user name and password are passed from command line, use fake email address to bypass login check
     docker login -u $REGISTRY_USERNAME -p "$REGISTRY_PASSWD" -e "@" $REGISTRY_SERVER:$REGISTRY_PORT
