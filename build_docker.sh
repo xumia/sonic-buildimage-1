547299c560d1d9e0fdd4598f4b0473ae03df61d9
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-04-06 10:34:23 -0700

    Make sure clean up return code zero

--- build_docker.sh
+++ build_docker.sh
@@ -20,6 +20,7 @@ REGISTRY_PASSWD=$5
 
 ## Docker image label, so no need to remember its hash
 docker_image_name=$DOCKER_BUILD_DIR
+remote_image_name=$REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name
 
 ## File name for docker image
 docker_image_gz=$docker_image_name.gz
@@ -32,7 +33,8 @@ docker_image_gz=$docker_image_name.gz
 function cleanup {
     rm -rf $DOCKER_BUILD_DIR/files
     rm -rf $DOCKER_BUILD_DIR/deps
-    docker images -q -f dangling=true | xargs --no-run-if-empty docker rmi | true
+    docker rmi $remote_image_name || true
+    docker rmi $docker_image_name || true
 }
 trap cleanup exit
 
@@ -61,12 +63,12 @@ fi
 if [ -n "$REGISTRY_SERVER" ] && [ -n "$REGISTRY_PORT" ]; then
     ## Add registry information as tag, so will push as latest
     ## Temporarily add -f option to prevent error message of Docker engine version < 1.10.0
-    docker tag -f $docker_image_name $REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name
+    docker tag -f $docker_image_name $remote_image_name
 
     ## Login the docker image registry server
     ## Note: user name and password are passed from command line, use fake email address to bypass login check
     docker login -u $REGISTRY_USERNAME -p "$REGISTRY_PASSWD" -e "@" $REGISTRY_SERVER:$REGISTRY_PORT
-    docker push $REGISTRY_SERVER:$REGISTRY_PORT/$docker_image_name
+    docker push $remote_image_name
 fi
 
 docker save $docker_image_name | gzip -c > $docker_image_gz
