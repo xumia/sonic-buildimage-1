34514cc6c41d3984a6347ed794265cb1ca386c96
Xudong Wu <xudongwu@microsoft.com>
2016-04-13 10:35:38 -0700

    Add docker file for build_router, and make minor modification on build_docker.sh Validated

--- build_docker.sh
+++ build_docker.sh
@@ -39,7 +39,7 @@ trap cleanup exit
 ## Copy dependencies
 ## Note: Dockerfile ADD doesn't support reference files outside the folder, so copy it locally
 mkdir -p $DOCKER_BUILD_DIR/deps
-cp deps/*.deb $DOCKER_BUILD_DIR/deps
+cp deps/* $DOCKER_BUILD_DIR/deps
 
 ## Copy the suggested Debian sources
 ## ref: https://wiki.debian.org/SourcesList
