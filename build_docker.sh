5245889d58e52e6703f57e4ab2199390bbd224c2
Qi Luo <qiluo-msft@users.noreply.github.com>
2016-04-26 11:16:14 -0700

    Revert "resolve the conflict on the merge"
    This reverts commit 578b02f8ab2f2c47a971b49efd6ff45fa23e4529, reversing
    changes made to 8be4b787411d663be29022f36c4bb9dc6ad6c146.

--- build_docker.sh
+++ build_docker.sh
@@ -33,15 +33,15 @@ docker_image_gz=$docker_image_name.gz
 function cleanup {
     rm -rf $DOCKER_BUILD_DIR/files
     rm -rf $DOCKER_BUILD_DIR/deps
-    docker rmi $docker_image_name || true
+    docker rmi $remote_image_name || true
 }
 trap cleanup exit
 
 ## Copy dependencies
 ## Note: Dockerfile ADD doesn't support reference files outside the folder, so copy it locally
-if ls deps/* 1>/dev/null 2>&1; then
+if ls deps/*.deb 1>/dev/null 2>&1; then
     mkdir -p $DOCKER_BUILD_DIR/deps
-    cp deps/* $DOCKER_BUILD_DIR/deps
+    cp deps/*.deb $DOCKER_BUILD_DIR/deps
 fi
 
 ## Copy the suggested Debian sources
@@ -49,6 +49,17 @@ fi
 mkdir -p $DOCKER_BUILD_DIR/files
 cp files/sources.list $DOCKER_BUILD_DIR/files
 docker build --no-cache -t $docker_image_name $DOCKER_BUILD_DIR
+
+## Flatten the image by importing an exported container on this image
+## Note: it will squash the image with only one layer and lost all metadata such as ENTRYPOINT,
+##       so apply only to the base image
+## TODO: wait docker-squash supporting Docker 1.10+
+## ref: https://github.com/jwilder/docker-squash/issues/45
+if [ "$docker_image_name" = "docker-base" ]; then
+    tmp_container=$(docker run -d ${docker_image_name} /bin/bash)
+    docker export $tmp_container | docker import - ${docker_image_name}
+    docker rm -f $tmp_container || true
+fi
 
 if [ -n "$REGISTRY_SERVER" ] && [ -n "$REGISTRY_PORT" ]; then
     ## Add registry information as tag, so will push as latest
