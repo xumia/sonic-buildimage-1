22be7486f13d3b64dc6a2b311f80d3d058cb247e
Guohan Lu <gulv@microsoft.com>
2016-07-23 02:02:31 +0000

    bug fix: fix variable name in docker orchagent

--- docker-orchagent/start.sh
+++ docker-orchagent/start.sh
@@ -11,7 +11,7 @@ PORTSYNCD_ARGS=""
 if [ "$onie_platform" == "x86_64-dell_s6000_s1220-r0" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
     PORTSYNCD_ARGS+="-p /etc/ssw/ACS-S6000/port_config.ini"
-elif [ "onie_platform" == "x86_64-mlnx_x86-r5.0.1400" ]; then
+elif [ "$onie_platform" == "x86_64-mlnx_x86-r5.0.1400" ]; then
     ORCHAGENT_ARGS+="-p /etc/ssw/ACS-MSN2700/port_config.ini"
 fi
 
