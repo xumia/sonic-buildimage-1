51089e73de40fa4eed7f5a556a08946a9277312d
John Arnold (AZURE) <johnar@microsoft.com>
2016-08-16 08:59:27 -0700

    Archive image using generated (build) image name

--- build_docker.sh
+++ build_docker.sh
@@ -125,4 +125,5 @@ fi
 
 mkdir -p target
 rm -f target/$docker_image_name.*.gz
-docker save $docker_image_name | gzip -c > target/$docker_image_name.$image_sha.gz
+docker save $docker_image_name | gzip -c > target/$docker_image_name.$build_version.gz
+echo "Image sha256: $image_sha"
