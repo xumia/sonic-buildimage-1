0dfbaa1a6f3f9062f5f02050ebbe0d6e8b7477dc
Guohan Lu <gulv@microsoft.com>
2016-09-08 00:16:15 +0000

    PR 48952: obfuscate the n3000 platform to m3000
     - obfuscate the n3000 platform to m3000

--- installer/x86_64/install.sh
+++ installer/x86_64/install.sh
@@ -55,9 +55,9 @@ ONIE_PLATFORM_EXTRA_CMDLINE_LINUX=""
 if [ "$onie_platform" == "x86_64-dell_s6000_s1220-r0" ]; then
     ONIE_PLATFORM_EXTRA_CMDLINE_LINUX="processor.max_cstate=1 intel_idle.max_cstate=0"
     `pwd`/dell-s6000-replace-reboot.sh
-elif [ "$onie_platform" == "x86_64-n3000-r0" ]; then
+elif [ "$onie_platform" == "x86_64-m3000-r0" ]; then
     ONIE_PLATFORM_EXTRA_CMDLINE_LINUX="acpi_enforce_resources=lax efi_no_storage_paranoia"
-    # Call script to set mac address for eth0 on n3000 platform 
+    # Call script to set mac address for eth0 on m3000 platform
     ./macset.sh
     if [ "$?" != "0" ]; then
         echo "./macset.sh exited with error, could not set mac-address correctly" >&2
