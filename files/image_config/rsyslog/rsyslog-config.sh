aa44fdac5d6665193c15cf4a76bc977fe3d9ab8f
Lawrence Lee <t-lale@microsoft.com>
2019-06-17 13:18:06 -0700

    [files]: add multi-instance support to logging
    * add rsyslog service template compatible with network namespaces
    * change rsyslog_config to use database instance unix socket
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>
    (cherry picked from commit e871f11b1269ff929dfda11f5c6b6d7d9600b3ca)

--- files/image_config/rsyslog/rsyslog-config.sh
+++ files/image_config/rsyslog/rsyslog-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sonic-cfggen -d -t /usr/share/sonic/templates/rsyslog.conf.j2 >/etc/rsyslog.conf
+sonic-cfggen -s /var/run/redis$1/redis.sock -d -t /usr/share/sonic/templates/rsyslog.conf.j2 >/etc/rsyslog$1.conf
 systemctl restart rsyslog
