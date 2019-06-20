f99839b7d5ed8e6d6ad0b428740c5ca6189449b7
Lawrence Lee <t-lale@microsoft.com>
2019-06-20 14:33:19 -0700

    [build_templates]: add multi-namespace spport to bgp
    * add bgp service template
    * create /etc/sonic/frr* for multiple instances of bgp docker container
    
    [frr docker]: add multi-ASIC support to bgpd.conf
    * add `redistribute connected` option to advertise routes to local interfaces
    * add `all` option to next-hop-self
    * add route-map to hide internal routes
    * add route-map to allow duplicate frontend router id
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>
    (cherry picked from commit 4ea01b738510aa652f5b5a56b0f2c50f15b6c221)

--- rules/docker-fpm-frr.mk
+++ rules/docker-fpm-frr.mk
@@ -24,7 +24,6 @@ SONIC_STRETCH_DBG_DOCKERS += $(DOCKER_FPM_FRR_DBG)
 $(DOCKER_FPM_FRR)_CONTAINER_NAME = bgp
 $(DOCKER_FPM_FRR)_RUN_OPT += --net=host --privileged -t
 $(DOCKER_FPM_FRR)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-$(DOCKER_FPM_FRR)_RUN_OPT += -v /etc/sonic/frr:/etc/frr:rw
 
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += vtysh:/usr/bin/vtysh
 $(DOCKER_FPM_FRR)_BASE_IMAGE_FILES += TSA:/usr/bin/TSA
