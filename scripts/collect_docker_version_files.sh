d71c4e143627d24bacd543a2d791c47277540f58
Ubuntu <xumia@jenkins-worker-102.k30btgut5wfupgvgrwvr05gqcd.xx.internal.cloudapp.net>
2020-10-10 13:41:03 +0000

    Fix bug

--- scripts/collect_docker_version_files.sh
+++ scripts/collect_docker_version_files.sh
@@ -10,6 +10,7 @@ TARGET_VERSIONS_PATH=$TARGET_PATH/versions/dockers/$DOCKER_IMAGE_NAME
 [ -d $TARGET_VERSIONS_PATH ] && rm -rf $TARGET_VERSIONS_PATH
 mkdir -p $TARGET_VERSIONS_PATH
 
+export DOCKER_CLI_EXPERIMENTAL=enabled
 docker run --name $DOCKER_CONTAINER --entrypoint /bin/bash $DOCKER_IMAGE
 docker cp -L $DOCKER_CONTAINER:/etc/os-release $TARGET_VERSIONS_PATH/
 docker cp -L $DOCKER_CONTAINER:/usr/local/share/buildinfo/diff-versions $TARGET_VERSIONS_PATH/
