79a87e649e478209b1da952564d34703087597b5
xumia <xumia@microsoft.com>
2020-10-18 15:00:28 +0000

    Fix bug

--- scripts/collect_build_version_files.sh
+++ scripts/collect_build_version_files.sh
@@ -5,9 +5,7 @@ BLDENV=$2
 TARGET_PATH=$3
 
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-BUILDINFO_PATH="/usr/local/share/buildinfo"
-POST_VERSION_PATH="$BUILDINFO_PATH/post-versions"
-
+. /usr/local/share/buildinfo/config/buildinfo.config
 
 [ -z "$BLDENV" ] && BLDENV=$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)
 [ -z "$TARGET_PATH" ] && TARGET_PATH=./target
@@ -16,10 +14,13 @@ VERSION_BUILD_PATH=$TARGET_PATH/versions/build
 VERSION_SLAVE_PATH=$VERSION_BUILD_PATH/sonic-slave-${BLDENV}
 LOG_VERSION_PATH=$VERSION_BUILD_PATH/log-${TIMESTAMP}
 
-sudo chmod a+rw $BUILDINFO_PATH
+sudo chmod -R a+rw $BUILDINFO_PATH
 collect_version_files $LOG_VERSION_PATH
+[ -d $BUILD_VERSION_PATH ] && cp -rf $BUILD_VERSION_PATH/* $LOG_VERSION_PATH/
 mkdir -p $VERSION_SLAVE_PATH
 
+
 scripts/versions_manager.py merge -t $VERSION_SLAVE_PATH -b $LOG_VERSION_PATH -e $POST_VERSION_PATH
+rm -rf $BUILD_VERSION_PATH/*
 
 exit $RET
