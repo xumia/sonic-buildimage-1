32fa8dcaeab0fda6a8a288150aeb7e286677543a
xumia <xumia@microsoft.com>
2020-10-26 13:36:09 +0000

    Support SONiC Reproduceable Build

---
+++ scripts/prepare_base_image_buildinfo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+ARCH=$1
+DISTRO=$2
+FILESYSTEM_ROOT=$3
+
+. /usr/local/share/buildinfo/config/buildinfo.config
+VERSION_DEB_PREFERENCE="01-versions-deb"
+BUILDINFO_PATH=${FILESYSTEM_ROOT}/usr/local/share/buildinfo
+BUILDINFO_INSTALL_PATH=${FILESYSTEM_ROOT}/usr/local/sbin
+BUILDINFO_VERSION_PATH=${FILESYSTEM_ROOT}/usr/local/share/buildinfo/versions
+BUILDINFO_VERSION_DEB=${BUILDINFO_VERSION_PATH}/${VERSION_DEB_PREFERENCE}
+OVERRIDE_VERSION_PATH=files/build/versions/host-image
+DIFF_VERSIONS_PATH=$BUILDINFO_PATH/diff-versions
+
+
+# Copy build info scripts
+mkdir -p $BUILDINFO_PATH
+cp -rf files/build/scripts ${BUILDINFO_PATH}/
+
+# Copy the build info config
+cp -rf files/build/buildinfo/* $BUILDINFO_PATH/
+
+# Install the config files
+cp -rf $BUILDINFO_PATH/scripts/* "$BUILDINFO_INSTALL_PATH/"
+cp $BUILDINFO_PATH/trusted.gpg.d/* "${FILESYSTEM_ROOT}/etc/apt/trusted.gpg.d/"
+
+# Generate version lock files
+scripts/versions_manager.py generate -t "$BUILDINFO_VERSION_PATH" -m "$OVERRIDE_VERSION_PATH" -d "$DISTRO" -a "$ARCH"
+
+if [ "$SONIC_ENABLE_VERSION_CONTROL" != "y" ] && [ -f $BUILDINFO_VERSION_DEB ]; then
+    cp -f $BUILDINFO_VERSION_DEB ${FILESYSTEM_ROOT}/etc/apt/preferences.d/
+fi
+
+sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "pre_run_buildinfo"
---
+++ scripts/prepare_docker_buildinfo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+IMAGENAME=$1
+DOCKERFILE=$2
+ARCH=$3
+DOCKERFILE_TARGE=$4
+DISTRO=$5
+
+[ -z "$DOCKERFILE_TARGE" ] && DOCKERFILE_TARGE=$DOCKERFILE
+DOCKERFILE_PATH=$(dirname "$DOCKERFILE_TARGE")
+BUILDINFO_PATH="${DOCKERFILE_PATH}/buildinfo"
+BUILDINFO_VERSION_PATH="${BUILDINFO_PATH}/versions"
+BUILDINFO_CONFIG=$BUILDINFO_PATH/scripts/buildinfo.config
+
+AUTO_GENERATE_CODE_TEXT="Begin Auto-Generated Code"
+DOCKERFILE_TEMPLATE="files/build/templates/dockerfile_auto_generate.j2"
+[ -d $BUILDINFO_PATH ] && rm -rf $BUILDINFO_PATH
+mkdir -p $BUILDINFO_VERSION_PATH
+
+# Get the debian distribution from the docker base image
+if [ -z "$DISTRO" ]; then
+    DOCKER_BASE_IMAGE=$(grep "^FROM" $DOCKERFILE | head -n 1 | awk '{print $2}')
+    DISTRO=$(docker run --rm --entrypoint "" $DOCKER_BASE_IMAGE cat /etc/os-release | grep VERSION_CODENAME | cut -d= -f2)
+    [ -z "$DISTRO" ] && DISTRO=jessie
+fi
+
+# Add the auto-generate code if it is not added in the target Dockerfile
+if [ ! -f $DOCKERFILE_TARGE ] || ! grep -q "$AUTO_GENERATE_CODE_TEXT" $DOCKERFILE_TARGE; then
+    # Insert the docker build script before the RUN command
+    LINE_NUMBER=$(grep -Fn -m 1 'RUN' $DOCKERFILE | cut -d: -f1)
+    DOCKERFILE_BEFORE_RUN_SCRIPT=$(generate_code="before_run" j2 $DOCKERFILE_TEMPLATE)
+    TEMP_FILE=$(mktemp)
+    awk -v text="${DOCKERFILE_BEFORE_RUN_SCRIPT}" -v linenumber=$LINE_NUMBER 'NR==linenumber{print text}1' $DOCKERFILE > $TEMP_FILE
+
+    # Append the docker build script at the end of the docker file
+    SET_ENV_PATH=y
+    [ "$BUILD_SLAVE" == "y" ] && SET_ENV_PATH=n
+    generate_code="after_run" set_env_path=$SET_ENV_PATH j2 $DOCKERFILE_TEMPLATE >> $TEMP_FILE
+
+    cat $TEMP_FILE > $DOCKERFILE_TARGE
+    rm -f $TEMP_FILE
+fi
+
+# Copy the build info config
+cp -rf files/build/buildinfo/* $BUILDINFO_PATH
+
+# Copy the docker build info scirpts
+cp -rf files/build/scripts "${BUILDINFO_PATH}/"
+
+# Build the slave running config
+if [ "$BUILD_SLAVE" == "y" ]; then
+    scripts/versions_manager.py generate -t "${BUILDINFO_PATH}/build/versions" -n "build-${IMAGENAME}" -d "$DISTRO" -a "$ARCH"
+fi
+
+# Generate the version lock files
+scripts/versions_manager.py generate -t "$BUILDINFO_VERSION_PATH" -n "$IMAGENAME" -d "$DISTRO" -a "$ARCH"
