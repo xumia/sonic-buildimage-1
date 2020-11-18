e93cf6a15852b803bf6539b376da75abbc76c06d
xumia <xumia@microsoft.com>
2020-11-18 05:58:33 +0000

    Remove j2 files

--- scripts/generate_buildinfo_config.sh
+++ scripts/generate_buildinfo_config.sh
@@ -4,19 +4,13 @@ BUILDINFO_PATH=files/build
 
 TRUSTED_GPG_PATH=$BUILDINFO_PATH/buildinfo/trusted.gpg.d
 BUILDINFO_CONFIG=$BUILDINFO_PATH/buildinfo/config/buildinfo.config
-BUILDINFO_CONFIG_TEMPLATE=$BUILDINFO_PATH/templates/buildinfo.config.j2
-
-AUTO_GENERATE_CODE_TEXT="Begin Auto-Generated Code"
-DOCKERFILE_TEMPLATE="files/build/templates/dockerfile_auto_generate.j2"
 
 [ -d $TRUSTED_GPG_PATH ] && rm -rf $TRUSTED_GPG_PATH
 mkdir -p $TRUSTED_GPG_PATH
 mkdir -p $BUILDINFO_PATH/buildinfo/config
 
-# Generate build info config file
-PACKAGE_URL_PREFIX=${PACKAGE_URL_PREFIX} \
-j2  $BUILDINFO_CONFIG_TEMPLATE > $BUILDINFO_CONFIG
-
+echo "PACKAGE_URL_PREFIX=$PACKAGE_URL_PREFIX" > $BUILDINFO_CONFIG
+echo "SONIC_VERSION_CONTROL_COMPONENTS=$SONIC_VERSION_CONTROL_COMPONENTS" >> $BUILDINFO_CONFIG
 
 
 # Download trusted gpgs
