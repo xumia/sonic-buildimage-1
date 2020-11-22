777e7f0d5d22404265b410f9768f6e1f9b7c5107
xumia <xumia@microsoft.com>
2020-11-22 04:03:27 +0000

    Fix bug

--- files/build/scripts/buildinfo_base.sh
+++ files/build/scripts/buildinfo_base.sh
@@ -47,57 +47,63 @@ check_if_url_exist()
     fi
 }
 
-verify_and_set_proxy_url()
-{
-    local index=$1
-    local url=$2
-    local version=
-    [ -f $WEB_VERSION_FILE ] && version=$(grep "^${url}=" $WEB_VERSION_FILE | awk -F"==" '{print $NF}')
-    if [ "$ENABLE_VERSION_CONTROL_WEB" != y ]; then
-        local real_version=$(get_url_version $url)
-        mkdir -p $LOG_PATH > /dev/null 2>&1
-        echo "$url==$real_version" >> ${BUILD_WEB_VERSION_FILE}
-    else
-        # When version control is enabled, check the version file, and set the proxy url
-        # If the proxy is not ready, it will verify the version of the current url
-        if [ -z "$version" ]; then
-            echo "Failed to verify the package: $url, the version is not specified" 2>&1
-            exit 1
-        fi
-
-        local filename=$(echo $url | awk -F"/" '{print $NF}' | cut -d? -f1 | cut -d# -f1)
-        local version_filename="${filename}-${version}"
-        local proxy_url="${PACKAGE_URL_PREFIX}/${version_filename}"
-        local url_exist=$(check_if_url_exist $proxy_url)
-        if [ "$url_exist" == y ]; then
-            COMMAND_PARAMETERS[$index]=$proxy_url
-        else
-            local real_version=$(get_url_version $url)
-            if [ "$real_version" != "$version" ]; then
-                echo "Failed to verify url: $url, real hash value: $real_version, expected value: $version_filename" 1>&2
-                exit 1
-            fi
-        fi
-    fi
-}
-
 download_packages()
 {
-    COMMAND_PARAMETERS=("$@")
-    for (( i=0; i<${#COMMAND_PARAMETERS[@]}; i++ ))
+    local parameters=("$@")
+    local filenames=
+    declare -A filenames
+    for (( i=0; i<${#parameters[@]}; i++ ))
     do
-        local para=${COMMAND_PARAMETERS[$i]}
+        local para=${parameters[$i]}
+        local nexti=$((i+1))
         if [[ "$para" == *://* ]]; then
-            verify_and_set_proxy_url $i "$para"
+            local url=$para
+            local real_version=
+            if [ "$ENABLE_VERSION_CONTROL_WEB" == y ]; then
+                local version=
+                local filename=$(echo $url | awk -F"/" '{print $NF}' | cut -d? -f1 | cut -d# -f1)
+                [ -f $WEB_VERSION_FILE ] && version=$(grep "^${url}=" $WEB_VERSION_FILE | awk -F"==" '{print $NF}')
+                if [ -z "$version" ]; then
+                    echo "Failed to verify the package: $url, the version is not specified" 2>&1
+                    exit 1
+                fi
+
+                local version_filename="${filename}-${version}"
+                local proxy_url="${PACKAGE_URL_PREFIX}/${version_filename}"
+                local url_exist=$(check_if_url_exist $proxy_url)
+                if [ "$url_exist" == y ]; then
+                    parameters[$i]=$proxy_url
+                    filenames[$version_filename]=$filename
+                    real_version=$version
+                else
+                    real_version=$(get_url_version $url)
+                    if [ "$real_version" != "$version" ]; then
+                        echo "Failed to verify url: $url, real hash value: $real_version, expected value: $version_filename" 1>&2
+                       exit 1
+                    fi
+                fi
+            else
+                real_version=$(get_url_version $url)
+            fi
+
+            echo "$url==$real_version" >> ${BUILD_WEB_VERSION_FILE}
         fi
     done
 
-    $REAL_COMMAND "${COMMAND_PARAMETERS[@]}"
+    $REAL_COMMAND "${parameters[@]}"
+    local result=$?
+
+    for filename in "${!filenames[@]}"
+    do
+        [ -f "$filename" ] && mv "$filename" "${filenames[$filename]}"
+    done
+
+    return $result
 }
 
 run_pip_command()
 {
-    COMMAND_PARAMETERS=("$@")
+    parameters=("$@")
 
     if [ ! -x "$REAL_COMMAND" ] && [ " $1" == "freeze" ]; then
         return 1
@@ -108,37 +114,28 @@ run_pip_command()
         return $?
     fi
 
-    local found=false
-    local install=false
+    local found=n
+    local install=n
     local pip_version_file=$PIP_VERSION_FILE
     local tmp_version_file=$(mktemp)
     [ -f "$pip_version_file" ] && cp -f $pip_version_file $tmp_version_file
-    for para in "${COMMAND_PARAMETERS[@]}"
+    for para in "${parameters[@]}"
     do
-        ([ "$para" == "-c" ] || [ "$para" == "--constraint" ]) && found=true
+        ([ "$para" == "-c" ] || [ "$para" == "--constraint" ]) && found=y
         if [ "$para" == "install" ]; then
-            install=true
+            install=y
         elif [[ "$para" == *.whl ]]; then
             package_name=$(echo $para | cut -d- -f1 | tr _ .)
             sed "/^${package_name}==/d" -i $tmp_version_file
-        elif [[ "$para" != -* ]]; then
-            package_name=$(echo $para | awk -F "[=><]" '{print $1}')
-            if ! grep -q "${package_name}==" $tmp_version_file; then
-                package_version=$(echo $para | awk -F "[=><]" '{for (i=1; i<NF; i++) {print $NF; break;}}')
-                if [ -z "$package_version" ]; then
-                    echo "Failed to install package: $package_name, the version is not specified." 1>&2
-                    return 1
-                fi
-            fi
         fi
     done
 
-    if [ "$found" == "false" ] && [ "$install" == "true" ]; then
-        COMMAND_PARAMETERS+=("-c")
-        COMMAND_PARAMETERS+=("${tmp_version_file}")
+    if [ "$found" == "n" ] && [ "$install" == "y" ]; then
+        parameters+=("-c")
+        parameters+=("${tmp_version_file}")
     fi
 
-    $REAL_COMMAND "${COMMAND_PARAMETERS[@]}"
+    $REAL_COMMAND "${parameters[@]}"
     local result=$?
     rm $tmp_version_file
     return $result
