d95992d6d70dcc06a1063cbb4502803f90c89734
Ubuntu <xumia@vmss-soni000009.k30btgut5wfupgvgrwvr05gqcd.xx.internal.cloudapp.net>
2021-01-02 04:52:41 +0000

    Fix bug

--- scripts/mirror/aptly.sh
+++ scripts/mirror/aptly.sh
@@ -191,7 +191,7 @@ update_repo()
 
     echo "Publish repos: $repos"
     if ! aptly -config $APTLY_CONFIG publish show $dist filesystem:debian: > /dev/null 2>&1; then
-        echo "aptly -config $APTLY_CONFIG publish repo -passphrase="***" -keyring=$GPG_FILE -distribution=$dist -architectures=$archs -component=$components $repos filesystem:debian:"
+        echo "aptly -config $APTLY_CONFIG publish repo -passphrase=*** -keyring=$GPG_FILE -distribution=$dist -architectures=$archs -component=$components $repos filesystem:debian:"
         aptly -config $APTLY_CONFIG publish repo -passphrase="$PASSPHRASE" -keyring=$GPG_FILE -distribution=$dist -architectures=$archs -component=$components $repos filesystem:debian:
     fi
 
